172cd8d170d08d4575307e9311f07d90d1503563
chris-schnitzer <62219119+chris-schnitzer@users.noreply.github.com>
2023-02-19 20:20:08 +0000

    ..

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ import logo from '../images/boardroom-logo.png';
 export default function Header() {
 	return(
 		<header>
-			<img className="logo" src={logo}/>
+			<img className="logo" src={logo} alt="The Board Room logo"/>
 		</header>
 	)
 }
