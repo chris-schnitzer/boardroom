8611d1f3673cfca7f90704cfd36673a5969b0261
chris-schnitzer <62219119+chris-schnitzer@users.noreply.github.com>
2023-02-20 22:12:30 +0000

    changed form layout and added options

--- src/pages/Select.js
+++ src/pages/Select.js
@@ -5,16 +5,51 @@ export default function Select() {
 		<div className="bg-select">	
 
 			<main className="select-wrap">
+			<p className="form-title">Please enter your details to get your shred on</p>
 			<form>
-				<label>Please enter your details to get your shred on</label>
-				<select>
-					<option value="">Height</option>
-				</select>
-				<select>
-					<option value="">Weight</option>
-				</select>
-				<select>
-					<option value="">Experience</option>
+				<label className="label-height">Height</label>
+				
+				<div className="height">
+					<select className="flex-item">
+						<option value="">Feet</option>
+						<option value="1">1</option>
+						<option value="2">2</option>
+						<option value="3">3</option>
+						<option value="4">4</option>
+						<option value="5">5</option>
+						<option value="6">6</option>
+						<option value="7">7</option>
+					</select>
+					<select className="flex-item">
+						<option value="">Inches</option>
+						<option value="1">1</option>
+						<option value="2">2</option>
+						<option value="3">3</option>
+						<option value="4">4</option>
+						<option value="5">5</option>
+						<option value="6">6</option>
+						<option value="7">7</option>
+						<option value="8">8</option>
+						<option value="9">9</option>
+						<option value="10">10</option>
+						<option value="11">11</option>
+					</select>
+				</div>
+
+
+
+				<label className="label-weight">Weight</label>
+				
+				<input type="text" name="weight"  autocomplete="off" placeholder="Weight in kilograms" />
+					
+
+
+
+				<label className="label-exp">Experience</label>
+				<select className="full-length">
+					<option value="">Beginner</option>
+					<option value="">Intermediate</option>
+					<option value="">Pro</option>
 				</select>
 				<button>Continue</button>
 			</form>
