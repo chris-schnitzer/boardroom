ddb9faef9b32c541d656b413d9f3f6e3a5eddc6e
chris-schnitzer <62219119+chris-schnitzer@users.noreply.github.com>
2023-10-05 21:53:48 +0100

    my-commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,16 +2,26 @@ import React, {useState} from 'react';
 import './Navbar.css';
 import { NavLink } from 'react-router-dom';
 
+import logo from '../images/boardroom-logo.svg';
+
 export default function Navbar() {
 	const[open, setOpen] = useState(false);
 	return(
-		<>
+		
+		<>	
+
+			
+
 			<div className="nav-menu-btn" onClick ={() => setOpen(!open)}>
 				{open ? <p>X</p> : <p>Menu</p>}
 			</div>
 
 			<nav className={open ? "nav-links-active" : "nav-links"}>
-				<div className="nav-wrapper">
+				<NavLink to="/" onClick={ () => setOpen(false) }>
+					<img className="nav-logo" src={logo} alt="The Board Room logo" />
+				</NavLink>
+					
+				<div className="nav-wrapper">	
 					<ul>
 						<li><NavLink to="/" className="nav-link" 
 						onClick={ () => setOpen(false) }
